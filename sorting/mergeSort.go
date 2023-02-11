aeb98b47bcda7612e9669564f0b116db0c93233e
Saul Burgos <saulburgos6@gmail.com>
2023-02-10 22:36:13 -0400

    go

---
+++ sorting/mergeSort.go
@@ -0,0 +1,26 @@
+package sorting
+
+func Merge(a, b []int) []int {
+	var r = make([]int, len(a)+len(b))
+	var i = 0
+	var j = 0
+
+	for i < len(a) && j < len(b) {
+		if a[i] <= b[j] {
+			r[i+j] = a[i]
+			i++
+		} else {
+			r[i+j] = b[j]
+			j++
+		}
+	}
+	for i < len(a) {
+		r[i+j] = a[i]
+		i++
+	}
+	for j < len(b) {
+		r[i+j] = b[j]
+		j++
+	}
+	return r
+}
